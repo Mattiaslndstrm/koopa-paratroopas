108351a6fdc38aedc55ba97f8068765f50c3f5fd
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-04-25 14:11:17 -0500

    add comments, remove semicolon

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-window.onload = function(); {
+//The frames in the sprite sheet is 16x16px
+//mario_wjlfy5.png
+
+window.onload = function() {
 
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext('2d');
