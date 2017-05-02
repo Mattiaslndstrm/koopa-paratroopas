0787b55066dd9a66a8fbdc2a3825eb8531d1c181
Peter Martinson <peter.j.martinson@gmail.com>
2017-05-01 22:21:11 -0400

    Begin adding background via array 'image'

--- public/src/background.js
+++ public/src/background.js
@@ -6,103 +6,90 @@
 */
 'use strict'
 
-var canvas = document.getElementById("game-canvas"),
-    ctx    = canvas.getContext("2d"),
+// canvas
+var DOM = {};
 
-    box_side = 10,
-    box_x = canvas.width/2,
-    box_y = canvas.height/2,
-    box_color = "#000000",
-    background_color = "#0000ff",
+DOM.$canvas = document.getElementById("game-canvas");
+var ctx     = DOM.$canvas.getContext("2d");
 
-    rightPressed = false,
-    leftPressed = false;
+// new Image()
+var landscape = new Image();
+landscape.src = "assets/landscape_tileset.png";
 
-// ============= Adding Character Objects!!!
+var tile = function (context, column, row, image) {
+  var that = {}; // why not just use `this`?
+  that.context = context;
+  that.width   = 16;
+  that.height  = 16;
+  that.column  = column;
+  that.row     = row;
+  that.image   = image;
 
-var Character = function(x, y, h, w, c) {
-  this.x = x;
-  this.y = y;
-  this.height = h;
-  this.width = w;
-  this.color = c;
-  console.log("new Character!!");
+  // context.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh) s=source, d=destination
+  that.render = function (x, y) {
+    that.context.drawImage(
+      that.image,
+      16 * that.column,
+      16 * that.row,
+      that.width,
+      that.height,
+      x,
+      y,
+      that.width,
+      that.height);
+  };
+
+  return that;
 }
 
-Character.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.rect(this.x, this.y, this.height, this.width);
-  ctx.fillStyle = this.color;
-  ctx.fill();
-  ctx.closePath();
-}
+// var tiles = {
+//   "floor" : tile( ctx, 0, 0, landscape),
+//   "brick" : tile( ctx, 1, 0, landscape),
+//   "question" : tile( ctx, 24, 0, landscape)
+// }
 
-var Peter = new Character(canvas.width/2, canvas.height/2, 20, 10, '#000000');
+var floor_tile = tile( ctx, 0, 0, landscape);
+var brick_tile = tile( ctx, 1, 0, landscape);
+var question_tile = tile( ctx, 24, 0, landscape);
+// context.drawImage
 
+// render -> drawImage
 
+var background = [
+'                ',
+'                ',
+'                ',
+'                ',
+'                ',
+'                ',
+'                ',
+'                ',
+'                ',
+'     bb?b?bb    ',
+'                ',
+'                ',
+'                ',
+'                ',
+'fffff   ffffffff',
+'fffff   ffffffff'
+];
 
-
-
-
-
-
-function drawBackground() {
-  ctx.beginPath();
-  ctx.rect(10, 10, canvas.width, canvas.height);
-  ctx.fillStyle = background_color;
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawGround() {
-  
-}
-
-function drawBox() {
-  ctx.beginPath();
-  ctx.rect(box_x, box_y, box_side, box_side);
-  ctx.fillStyle = box_color;
-  ctx.fill();
-  ctx.closePath();
-}
-
-
-
-
-function draw() {
-  drawBackground();
-  // drawBox();
-  Peter.draw();
-
-  // move the box
-  if ( rightPressed && box_x < canvas.width ) {
-    Peter.x += 4;
-  }
-  else if ( leftPressed && box_x > 0 ) {
-    Peter.x -= 4;
-  }
-}
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-  if (e.keyCode == 39) {
-    rightPressed = true;
-  }
-  else if (e.keyCode == 37) {
-    leftPressed = true;
-  }
-}
-
-function keyUpHandler(e) {
-  if (e.keyCode == 39) {
-    rightPressed = false;
-  }
-  else if (e.keyCode == 37) {
-    leftPressed = false;
+for (var i = 0; i <= 16; i++) {
+  for (var j = 0; j <= 16; j++) {
+    switch (background[i].substring(j,j+1)) {
+    case 'f':
+      floor_tile.render(j*16, i*16);
+      break;
+    case 'b':
+      brick_tile.render(j*16, i*16);
+      break;
+    case '?':
+      question_tile.render(j*16, i*16);
+      break;
+    default:
+      break;
+    }
   }
 }
 
 
-setInterval(draw, 10);
