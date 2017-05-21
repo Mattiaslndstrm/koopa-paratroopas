d6f7b2df4583e5d8831fb57f748bdd67eeeddcb5
Peter Martinson <peter.j.martinson@gmail.com>
2017-05-21 12:26:26 -0400

    Place Mario and Goomba in World
    Still not working:
    1. Collisions with objects
    2. Collisions with monsters
    3. World Scrolling
    
    Also, Mario's a little buggy.  Sometimes he won't move...

--- src/mario.js
+++ src/mario.js
@@ -180,9 +180,9 @@ function sprite (options) {
     // colliosion happens, and update that with the x-coordinates of the 
     // object. No idea if it will work, but let's try it!
     that.collisionDetection = function() {
-        if (that.y >= canvas.height - 148) {
+        if (that.y >= (canvas.height - 16) -2*16) {
             that.onground = true;
-            that.y = canvas.height - 148;
+            that.y = (canvas.height - 16) -2*16;
         }
     };
 
@@ -240,7 +240,7 @@ var mario = sprite({
     numberOfFrames: 4,
     ticksPerFrame: 8,
     x: 20,
-    y: canvas.height - 148,
+    y: (canvas.height - 16) -2*16,
     velocityX: 1.5,
     velocityY: 0,
     gravity: 0.3,
